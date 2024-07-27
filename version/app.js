3226e4a95f18d65cbaf0b0dd71926c8548831b76
abdeaziz99 <abdelaziz99>
2024-07-27 20:49:40 +0000

    Update app to v3

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app. By Azdia' + '\n';
+    response = 'This is version 3 of the app. By Azdia' + '\n';
 
     //send the response to the client
     res.send(response);
